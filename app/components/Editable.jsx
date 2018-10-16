d4c7764951c8c42955dd18c9376de97b5daa6115
dong <gengyudonga@gmail.com>
2018-10-16 14:52:54 +0800

    Styling the Notes Application

--- app/components/Editable.jsx
+++ app/components/Editable.jsx
@@ -1,22 +1,26 @@
-import React from 'react'
+import React from 'react';
+
+import classnames from 'classnames';
+
 
 // We could allow edit/value to be swapped here through props
-const Editable = ({ editing, value, onEdit }) => {
+const Editable = ({ editing, value, onEdit, className, ...props }) => {
   if (editing) {
-    return <Editable.Edit value={value} onEdit={onEdit} />
+    return <Editable.Edit className={className} value={value} onEdit={onEdit} />
   }
-  return <Editable.Value value={value} />
+  return <Editable.Value className={classnames('value', className)} value={value} />
 }
 
 Editable.Value = ({ value, ...props }) => <span {...props}>{value}</span>
 
 class Edit extends React.Component {
   render() {
-    const { value, onEdit, ...props } = this.props
+    const { className, value, onEdit, ...props } = this.props
 
     return (
       <input
         type="text"
+        className={classnames('edit', className)}
         autoFocus={true}
         defaultValue={value}
         onBlur={this.finishEdit}
