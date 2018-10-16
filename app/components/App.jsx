7160f2996fa7a41020db1c6b25c1528194328334
dong <gengyudonga@gmail.com>
2018-10-16 17:40:12 +0800

    Handling Data Dependencies

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -5,93 +5,83 @@ import Notes from './Notes';
 import connect from '../libs/connect';
 import NoteActions from '../actions/NoteActions.js'
 
-// @connect(({lanes}) => ({lanes}), {
-//   laneActions: LaneActions
-// })
+import Lanes from './Lanes';
+import LaneActions from '../actions/LaneActions';
 
-class App extends React.Component {
-  render() {
-    const {notes} = this.props;
-    return (
-      <div>
-        <button className="add-note" onClick={this.addNote}>+</button>
-        <Notes
-          notes={notes}
-          onNoteClick={this.activateNoteEdit}
-          onEdit={this.editNote}
-          onDelete={this.deleteNote}
-          />
-      </div>
-    );
-  }
-
-  addNote = () => {
-    // It would be possible to write this in an imperative style.
-    // I.e., through `this.state.notes.push` and then
-    // `this.setState({notes: this.state.notes})` to commit.
-    //
-    // I tend to favor functional style whenever that makes sense.
-    // Even though it might take more code sometimes, I feel
-    // the benefits (easy to reason about, no side effects)
-    // more than make up for it.
-    //
-    // Libraries, such as Immutable.js, go a notch further.
-
-    // this.setState({
-    //   notes: this.state.notes.concat([{
-    //     id: uuid.v4(),
-    //     task: 'New task'
-    //   }])
-    // });
-    this.props.NoteActions.create({
+const App = ({LaneActions, lanes}) => {
+  const addLane = () => {
+    LaneActions.create({
       id: uuid.v4(),
-      task: 'New task'
+      name: 'New lane'
     });
-  }
+  };
 
-  deleteNote = (id, e) => {
-    // Avoid bubbling to edit
-    e.stopPropagation();
+  return (
+    <div>
+      <button className="add-lane" onClick={addLane}>+</button>
+      <Lanes lanes={lanes} />
+    </div>
+  );
+};
 
-    // this.setState({
-    //   notes: this.state.notes.filter(note => note.id !== id)
-    // });
-
-    this.props.NoteActions.delete(id);
-  }
-
-  activateNoteEdit = (id) => {
-    // this.setState({
-    //   notes: this.state.notes.map(note => {
-    //     if(note.id === id) {
-    //       note.editing = true;
-    //     }
-
-    //     return note;
-    //   })
-    // });
-
-    this.props.NoteActions.update({id, editing: true});
-  }
-
-  editNote = (id, task) => {
-    // this.setState({
-    //   notes: this.state.notes.map(note => {
-    //     if(note.id === id) {
-    //       note.editing = false;
-    //       note.task = task;
-    //     }
-
-    //     return note;
-    //   })
-    // });
-
-    this.props.NoteActions.update({id, task, editing: false});
-  }
-}
-
-export default connect(({notes}) => ({
-  notes
+export default connect(({lanes}) => ({
+  lanes
 }), {
-  NoteActions
+  LaneActions
 })(App)
+
+
+
+// class App extends React.Component {
+//   render() {
+//     const {notes} = this.props;
+//     return (
+//       <div>
+//         <button className="add-note" onClick={this.addNote}>+</button>
+//         <Notes
+//           notes={notes}
+//           onNoteClick={this.activateNoteEdit}
+//           onEdit={this.editNote}
+//           onDelete={this.deleteNote}
+//           />
+//       </div>
+//     );
+//   }
+
+//   addNote = () => {
+//     // It would be possible to write this in an imperative style.
+//     // I.e., through `this.state.notes.push` and then
+//     // `this.setState({notes: this.state.notes})` to commit.
+//     //
+//     // I tend to favor functional style whenever that makes sense.
+//     // Even though it might take more code sometimes, I feel
+//     // the benefits (easy to reason about, no side effects)
+//     // more than make up for it.
+//     //
+//     // Libraries, such as Immutable.js, go a notch further.
+//     this.props.NoteActions.create({
+//       id: uuid.v4(),
+//       task: 'New task'
+//     });
+//   }
+
+//   deleteNote = (id, e) => {
+//     // Avoid bubbling to edit
+//     e.stopPropagation();
+//     this.props.NoteActions.delete(id);
+//   }
+
+//   activateNoteEdit = (id) => {
+//     this.props.NoteActions.update({id, editing: true});
+//   }
+
+//   editNote = (id, task) => {
+//     this.props.NoteActions.update({id, task, editing: false});
+//   }
+// }
+
+// export default connect(({notes}) => ({
+//   notes
+// }), {
+//   NoteActions
+// })(App)
